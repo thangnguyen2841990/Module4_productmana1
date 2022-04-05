97b811b337fa3285c18ecd0acb61cd0f6fdecdd8
thangnguyen2841990 <96616281+thangnguyen2841990@users.noreply.github.com>
2022-04-05 11:51:49 +0700

    done

--- src/main/java/com/codegym/controller/ProductController.java
+++ src/main/java/com/codegym/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.codegym.controller;
 
 import com.codegym.model.Product;
+import com.codegym.model.ProductForm;
 import com.codegym.service.product.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,9 +11,11 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.FileCopyUtils;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.Valid;
 import java.io.File;
 import java.io.IOException;
 import java.util.Optional;
@@ -52,26 +55,50 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<Product> createProduct(@RequestPart("image") MultipartFile image,@RequestPart Product product) {
-    String fileName = image.getOriginalFilename();
-    long currentTime = System.currentTimeMillis();
-    fileName = currentTime + fileName;
+    public ResponseEntity<Product> createProduct(@Valid @ModelAttribute ProductForm productForm, BindingResult bindingResult) {
+        if (bindingResult.hasFieldErrors()){
+            return new ResponseEntity<>(HttpStatus.FOUND);
+        }
+        MultipartFile image = productForm.getImage();
+        String fileName = image.getOriginalFilename();
+        long currentTimeMillis = System.currentTimeMillis();
+        fileName = currentTimeMillis + fileName;
         try {
             FileCopyUtils.copy(image.getBytes(), new File(uploadPath + fileName));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        product.setImage(fileName);
-        return new ResponseEntity<>(productService.save(product), HttpStatus.CREATED);
+        Product product = new Product(productForm.getName(), productForm.getPrice(), productForm.getQuantity(),
+                productForm.getDescription(), fileName, productForm.getCategory());
+        return new ResponseEntity<>(this.productService.save(product), HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Product> editProduct(@PathVariable Long id, @RequestBody Product product) {
+    public ResponseEntity<Product> editProduct( @PathVariable Long id,@Valid @ModelAttribute ProductForm productForm,BindingResult bindingResult) {
         Optional<Product> productOptional = this.productService.findById(id);
         if (!productOptional.isPresent()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        product.setId(productOptional.get().getId());
+        if (bindingResult.hasFieldErrors()){
+            return new ResponseEntity<>(HttpStatus.FOUND);
+        }
+        MultipartFile multipartFile = productForm.getImage();
+        String image;
+        if (multipartFile.getSize() == 0) {
+            image = productOptional.get().getImage();
+        } else {
+            String fileName = productForm.getImage().getOriginalFilename();
+            long currentTime = System.currentTimeMillis();
+            fileName = currentTime + fileName;
+            image = fileName;
+            try {
+                FileCopyUtils.copy(productForm.getImage().getBytes(), new File(uploadPath + fileName));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        Product product = new Product(productOptional.get().getId(), productForm.getName(), productForm.getPrice(), productForm.getQuantity(),
+                productForm.getDescription(), image, productForm.getCategory());
         return new ResponseEntity<>(this.productService.save(product), HttpStatus.OK);
     }
 
@@ -84,13 +111,14 @@ public class ProductController {
         this.productService.remove(id);
         return new ResponseEntity<>(productOptional.get(), HttpStatus.NO_CONTENT);
     }
+
     @GetMapping("/search/{id}")
-    public ResponseEntity<Page<Product>> findByCategory(@PathVariable Long id, @PageableDefault(value = 5) Pageable pageable){
-        Page<Product> products = this.productService.findByCategory(id,pageable);
-        if (products.isEmpty()){
+    public ResponseEntity<Page<Product>> findByCategory(@PathVariable Long id, @PageableDefault(value = 5) Pageable pageable) {
+        Page<Product> products = this.productService.findByCategory(id, pageable);
+        if (products.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
-        return  new ResponseEntity<>(products, HttpStatus.OK);
+        return new ResponseEntity<>(products, HttpStatus.OK);
     }
 
 }
